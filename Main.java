0946edd40ed79dc193930be14a6294caeb98e805
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-21 21:17:55 +0530

    main file

---
+++ Main.java
@@ -0,0 +1,7 @@
+package classProblemByAmolMateSir.Day14PracticeProblem.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to LinkedList");
+    }
+}
